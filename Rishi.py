dd1c81dfc28318438905b89968862a708bfd7830
Rishi2101 <rushikeshsharma1188@gmail.com>
2022-07-05 18:22:08 +0530

    first commit

---
+++ Rishi.py
@@ -0,0 +1,77 @@
+#type checking
+#a = 10
+#b = 3.14
+#c = 5+4j
+
+#print(type(a))
+#print(type(b))
+#print(type(c))
+
+#type conversion
+#int to binary
+#d = bin(a)
+#print(d)
+
+#bin to int
+#e = 0b1010
+#print(int(e))
+
+#integer to Octal
+#print(oct(a))
+
+#octal to integer
+#print(int(0o12))
+
+
+#string are anything in python include in single or double quotes.
+#str = 'Yasham Academy'
+#print(type(str))
+
+#Write a Python program that accepts an integer (n) and
+#computes the value of n+nn+nnn
+
+#n = int(input("Enter The Value Of n ="))
+#print(n+n*n+n*n*n)
+
+
+#15
+#sand foundary test
+
+#string are anything in python include in single or double quotes.
+#operation on String
+#indexing
+
+#str1 = 'Yasham Academy'
+#print(type(str1))
+
+#print(str1[0])
+#print(str1[-14])
+
+#slicing
+#print(str1[0:6])
+#print(str1[-14:-8])        #n= n-1
+#print(str1[0:6:-1])
+#print(str1[6:0])
+#print(str1[-14:-8:-1])
+#print(str1[-8:-14])
+#print(str1[::-1])
+
+
+#step size
+#print(str1[0: :3])
+
+#concationation
+#str2 = 'is a Good'
+#print(str1 + str2)
+#print(str1 , str2)
+
+#w3resource
+#Q1
+#Str3 = "Twinkle, twinkle, little star, \n\t How I wonder what you are! \n\t\t Up above the world so high, \n\t\t Like a diamond in the sky. \n Twinkle, twinkle, little star, \n\t How I wonder what you are"
+#print(Str3)
+
+#Q5
+n1 =input('Enter The First Name =')
+n2 =input('Enter The Last Name =')
+print(n2,n1)
+
